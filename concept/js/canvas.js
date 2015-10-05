29288ac81ebce590eeca74850d832ea6ccd26b23
Jasper Meyer <jasper.meyer@live.de>
2015-10-05 14:47:52 +0200

    initial commit. first steps of concept included.

---
+++ concept/js/canvas.js
@@ -0,0 +1,7 @@
+var drawingCanvas = document.getElementById('roadmap-drawing');
+// Check the element is in the DOM and the browser supports canvas
+if(drawingCanvas.getContext) {
+// Initaliase a 2-dimensional drawing context
+var context = drawingCanvas.getContext('2d');
+//Canvas commands go here
+}
